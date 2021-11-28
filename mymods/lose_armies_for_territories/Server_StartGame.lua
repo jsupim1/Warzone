3d91620508f98c366f2e06633865618817425d60
jsupim1 <95108694+jsupim1@users.noreply.github.com>
2021-11-28 23:23:04 +0000

    Delete mymods directory

--- mymods/lose_armies_for_territories/Server_StartGame.lua
+++
@@ -1,9 +0,0 @@
-require "LoseArmiesForTerritories"
-
--- set initial Mod data (auto and manual dist) - called in manual and auto dist
--- trying to do in maul dist using Server_StartDistrubtion causes standing.NumResources to be nil
-
-function Server_StartGame(game, standing)
-	print("init Server_StartGame");
-	SetInitialStorage(game);
-end
